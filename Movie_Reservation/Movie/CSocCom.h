bc77df8404c3e67e48cbb1473a47911822bba36e
manduMY <poi31320@gmail.com>
2020-02-15 10:26:46 +0900

    UPDATES readme

--- Movie_Reservation/Movie/CSocCom.h
+++ Movie_Reservation/Movie/CSocCom.h
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cf5130869220fb41afb5dd908f884311575e7a93bdd53418c49769f2442fac66
-size 172
+#pragma once
+class CSocCom : public CSocket
+{
+	HWND m_hWnd;
+public:
+	CSocCom();
+	~CSocCom();
+	virtual void OnReceive(int nErrorCode);
+	void Init(HWND hWnd);
+};
+
--- Movie_Reservation/Movie/DlgRank.h
+++ Movie_Reservation/Movie/DlgRank.h
@@ -1,3 +1,31 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dad7d67987450ef90f1ddfa01c086d1fe8c9bc1005975cf6f3bebd4eef3463da
-size 864
+﻿#pragma once
+
+
+// CDlgRank 대화 상자
+
+class CDlgRank : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDlgRank)
+
+public:
+	CDlgRank(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CDlgRank();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG_RANK };
+#endif
+
+protected:
+	void SetSearchResult();
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	CListCtrl m_RankList;			 // 영화의 정보들을 담을 리스트컨트롤
+	virtual BOOL OnInitDialog();	
+	CStatic m_Disaplay;				 // 영화 포스터를 나타낼 컨트롤
+	afx_msg void OnClickedButton5(); // 각 영화의 사진을 처리하는 함수
+	CString m_strname;				 // 영화 이름에 관한 Edit 컨트롤
+	afx_msg void OnStnClickedStaticMoviepicture();
+};
